85fd7f0958135af95a6b0ef3db74a934b2d4abdf
FofuxoSibov <danilo.sibov@gmail.com>
2023-05-23 08:33:54 -0300

    exemplo-aula2

---
+++ Aula 2 - Variavel Inteiro/VariavelFloat.py
@@ -0,0 +1,8 @@
+#Variaveis inteiras
+num1 = float(input ("Digite o primeiro valor: "))
+num2 = float(input ("Digite o segundo valor: "))
+
+totalsoma = num1 + num2
+
+#Imprimir o resultado
+print (totalsoma)
---
+++ Aula 2 - Variavel Inteiro/VariavelInteiro.py
@@ -0,0 +1,8 @@
+#Variaveis inteiras
+num1 = int(input ("Digite o primeiro valor: "))
+num2 = int(input ("Digite o segundo valor: "))
+
+totalsoma = num1 + num2
+
+#Imprimir o resultado
+print (totalsoma)
